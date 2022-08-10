8b12c861c8a1cecd83184ae6487539cc8b8b969d
viethoang2810 <anhdung19022001@email.com>
2022-08-10 13:10:59 +0700

    Process form login by using Formik

---
+++ src/components/ChatRoom/index.js
@@ -0,0 +1,7 @@
+function ChatRoom() {
+    return ( 
+        <h2>Welcome to Chat Room</h2>
+     );
+}
+
+export default ChatRoom;
--- src/components/Form/Login/index.js
+++ src/components/Form/Login/index.js
@@ -4,30 +4,47 @@ import Button from "../../Button/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faGooglePlus } from "@fortawesome/free-brands-svg-icons";
 import { authentication } from "../../Firbase/Config";
-import { signInWithPopup , FacebookAuthProvider } from "firebase/auth";
-
+import { signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const cx = classNames.bind(styles);
 
 function Login() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+
+
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string()
+                .required("Required")
+                .min(7,'Must be contain at least 7 letters'),
+      password: Yup.string().required("Required"),
+    }),
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
   const loginWithFacebook = () => {
     const provider = new FacebookAuthProvider();
-    signInWithPopup(authentication,provider)
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  }
+    signInWithPopup(authentication, provider)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  console.log(formik.errors.username);
   return (
     <div className={cx("wrapper")}>
       <h5 className={cx("form-title")}>Login</h5>
-      <form onSubmit={handleSubmit} className={cx("form")}>
+      <form onSubmit={formik.handleSubmit} className={cx("form")}>
         <div className={cx("input-info")}>
           <label>Username</label>
           <input
@@ -35,7 +52,10 @@ function Login() {
             name="username"
             id="username"
             placeholder="Enter username ...."
+            onChange={formik.handleChange}
+            value={formik.values.username}
           />
+          {formik.errors.username ? (<p className={cx('errorMsg')}>{formik.errors.username}</p>) : null}
         </div>
         <div className={cx("input-info")}>
           <label>Password</label>
@@ -44,16 +64,22 @@ function Login() {
             name="password"
             id="password"
             placeholder="Enter password ...."
+            onChange={formik.handleChange}
+            value={formik.values.password}
           />
+          {formik.errors.password ? (<p className={cx('errorMsg')}>{formik.errors.password}</p>) : null}
         </div>
         <Button primary children="Login" />
         <h5 className={cx("alternative-login")}>Or login with</h5>
         <div className={cx("alternative-social-media")}>
-          <button className={cx("btn-login", "google-login")} >
+          <button className={cx("btn-login", "google-login")}>
             <span>Login with Google</span>
             <FontAwesomeIcon icon={faGooglePlus} className={cx("google-dep")} />
           </button>
-          <button className={cx("btn-login", "facebook-login")} onClick={loginWithFacebook}>
+          <button
+            className={cx("btn-login", "facebook-login")}
+            onClick={loginWithFacebook}
+          >
             <span>Login with Facebook</span>
             <FontAwesomeIcon icon={faFacebook} className={cx("facebook-dep")} />
           </button>
