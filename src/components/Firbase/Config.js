04cb38c10ce61008344660c9341f760b761eaadb
viethoang2810 <anhdung19022001@email.com>
2022-08-10 10:52:57 +0700

    Update login with Facebook feature

---
+++ src/components/Firbase/Config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5jmlm6Wl00pwN-d733NX1b37kQbhf8rI",
+  authDomain: "messenger-fa7e6.firebaseapp.com",
+  projectId: "messenger-fa7e6",
+  storageBucket: "messenger-fa7e6.appspot.com",
+  messagingSenderId: "454742769396",
+  appId: "1:454742769396:web:c28f11e5f973d64a3c68a1",
+  measurementId: "G-EZ5VKJXK7E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const authentication = getAuth(app);
--- src/components/Form/Login/index.js
+++ src/components/Form/Login/index.js
@@ -1,39 +1,66 @@
-import classNames from 'classnames/bind';
-import styles from './Login.module.scss' ;
-import Button from '../../Button/Button'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faGoogle, faGooglePlus } from '@fortawesome/free-brands-svg-icons';
+import classNames from "classnames/bind";
+import styles from "./Login.module.scss";
+import Button from "../../Button/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faGooglePlus } from "@fortawesome/free-brands-svg-icons";
+import { authentication } from "../../Firbase/Config";
+import { signInWithPopup , FacebookAuthProvider } from "firebase/auth";
+
 
 const cx = classNames.bind(styles);
 
 function Login() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    }
-
-    return ( 
-        <div className={cx('wrapper')}>
-            <h5 className={cx('form-title')}>Login</h5>
-            <form onSubmit={handleSubmit} className={cx('form')}>
-                <div className={cx('input-info')}>
-                    <label>Username</label>
-                    <input type="text" name="username" id="username" placeholder="Enter username ...."/>
-                </div>
-                <div className={cx('input-info')}>
-                    <label>Password</label>
-                    <input type="password" name="password" id="password" placeholder="Enter password ...."/>
-                </div>
-                <Button primary children="Login"/>
-                <h5  className={cx('alternative-login')}>Or login with</h5>
-                <div className={cx('alternative-social-media')}>
-                    <FontAwesomeIcon icon={faGooglePlus} className={cx('google-dep')}/>
-                    <FontAwesomeIcon icon={faFacebook} className={cx('facebook-dep')}/>
-                </div>
-            </form>
+  const loginWithFacebook = () => {
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(authentication,provider)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+  return (
+    <div className={cx("wrapper")}>
+      <h5 className={cx("form-title")}>Login</h5>
+      <form onSubmit={handleSubmit} className={cx("form")}>
+        <div className={cx("input-info")}>
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter username ...."
+          />
         </div>
-     );
+        <div className={cx("input-info")}>
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Enter password ...."
+          />
+        </div>
+        <Button primary children="Login" />
+        <h5 className={cx("alternative-login")}>Or login with</h5>
+        <div className={cx("alternative-social-media")}>
+          <button className={cx("btn-login", "google-login")} >
+            <span>Login with Google</span>
+            <FontAwesomeIcon icon={faGooglePlus} className={cx("google-dep")} />
+          </button>
+          <button className={cx("btn-login", "facebook-login")} onClick={loginWithFacebook}>
+            <span>Login with Facebook</span>
+            <FontAwesomeIcon icon={faFacebook} className={cx("facebook-dep")} />
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
